188796052689687fdc74d920a39a224fb834572f
Eremenko_Dabila <test_email@yandex.ru>
2024-11-09 15:02:00 +0300

    Cmake работает

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 
 using namespace std;
 
 int main(){
 
-cout<<"second commite";
-cout<<"4";
-cout<<"5";
+
+//std::string filename = "test.txt";
+std::string filename = "test.txt";
+
+std::ifstream file(filename);
+
+  if (!file.is_open()) {
+        std::cerr << "Erorr to openb " << filename << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    file >> word;
+    
+    file.close();
+    
+    std::cout << "Ecsample  " << word << std::endl;
 }
