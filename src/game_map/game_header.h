c9a4a41e933b97013c351a7b6efdaf535c8aa071
Alexand Naid <alexandr.naidenko@mail.ru>
2024-12-07 18:16:46 +0300

    путь

--- src/game_map/game_header.h
+++ src/game_map/game_header.h
@@ -1,7 +1,20 @@
-#ifndef game_header
-#define game_header
+#ifndef game_map
+#define game_map
 
+#include <SFML/Graphics.hpp>>
+#include <string>
 
+class GameMap {
+public:
+    GameMap(const std::string& mapFilePath, const std::string& backgroundFilePath);
+    void displayMap(sf::RenderWindow& window);
 
+private:
+    sf::Texture texture; // Текстура карты
+    sf::Sprite sprite;   // Спрайт карты
 
-#endif
+    sf::Texture backgroundTexture; // Текстура фона
+    sf::Sprite backgroundSprite;   // Спрайт фона
+};
+
+#endif // game_map
