9c147ef73bfd8a6ad50cd8ea769144ecf938f4fd
Alexand Naid <alexandr.naidenko@mail.ru>
2024-12-07 18:16:46 +0300

    путь

---
+++ src/game_map/game_map.cpp
@@ -0,0 +1,30 @@
+#include "game_header.h"
+#include <iostream>
+
+GameMap::GameMap(const std::string& mapFilePath, const std::string& backgroundFilePath) {
+    // Загружаем текстуру фона
+    if (!backgroundTexture.loadFromFile(backgroundFilePath)) {
+        std::cerr << "Error: Could not load background image." << std::endl;
+    } else {
+        backgroundSprite.setTexture(backgroundTexture);
+        // Устанавливаем размер фона по размеру окна
+        backgroundSprite.setScale(
+            static_cast<float>(800) / backgroundTexture.getSize().x,
+            static_cast<float>(600) / backgroundTexture.getSize().y
+        );
+    }
+
+    // Загружаем текстуру карты
+    if (!texture.loadFromFile(mapFilePath)) {
+        std::cerr << "Error: Could not load map image." << std::endl;
+    } else {
+        sprite.setTexture(texture);
+    }
+}
+
+void GameMap::displayMap(sf::RenderWindow& window) {
+    // Отображаем фон
+    window.draw(backgroundSprite);
+    // Отображаем карту поверх фона
+    window.draw(sprite);
+}
