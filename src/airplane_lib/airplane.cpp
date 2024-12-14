4850bb0cb9041ea427787d16f099443b1ed426a5
Alexand Naid <alexandr.naidenko@mail.ru>
2024-12-14 13:26:39 +0300

    1

--- src/airplane_lib/airplane.cpp
+++ src/airplane_lib/airplane.cpp
@@ -6,20 +6,20 @@ void moveSprite_all(sf::Sprite& sprite, int speed, sf::Vector2u size,unsigned in
     //переделать под длину 
     sf::Vector2f position = sprite.getPosition();
     // Получаем состояние клавиш
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) and (position.y > 0 )) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && (position.y > 0 )) {
         // Перемещаем спрайт вверх
         sprite.move(0.f, -speed);  // Изменение координаты Y
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) and (position.y < (window_height-(window_height*0.01))-(height/2))) 
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && (position.y < (window_height-(window_height*0.01))-(height/2))) 
     {
         // Перемещаем спрайт вниз
         sprite.move(0.f,speed);   // Изменение координаты Y
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) and (position.x > (width/2) )) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && (position.x > (width/2) )) {
         // Перемещаем спрайт влево
         sprite.move(-speed, 0.f);  // Изменение координаты X
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) and (position.x < (window_width-(window_width*0.01)) )) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && (position.x < (window_width-(window_width*0.01)) )) {
         // Перемещаем спрайт вправо
         sprite.move(speed, 0.f);   // Изменение координаты X
     }
